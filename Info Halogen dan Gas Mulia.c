322e7cfcad7ae4bdce7b6376c391181016213830
Rei911 <150983149+Rei911@users.noreply.github.com>
2023-11-18 22:01:12 +0700

    Add files via upload

---
+++ Info Halogen dan Gas Mulia.c
@@ -0,0 +1,96 @@
+				
+				Halogen
+				
+				
+				Nama : Fluorin
+				Simbol : F
+				Nomor Atom : 9
+				Massa Atom : 18,998 u
+				konfigurasi elektron : 1s^2 2s^2 2p^5
+				Titik lebur : 53,48 K
+				Titik didih : 85,03 K
+				
+				Nama : Klorin
+				Simbol : Cl
+				Nomor Atom : 17
+				Massa Atom : 35,45 u
+				konfigurasi elektron : [Ne] 3s^2 3p^5
+				Titik lebur : 171,6 K
+				Titik didih : 239,11 K
+				
+				Nama : Bromin
+				Simbol : B
+				Nomor Atom : 35
+				Massa Atom : 79,904 u
+				konfigurasi elektron : [Ar] 3d^10 4s^2  4p^5
+				Titik lebur : 265,8 K
+				Titik didih : 332,0 K
+				
+				Nama : Iodin
+				Simbol : I
+				Nomor Atom : 53
+				Massa Atom : 126,90 u
+				konfigurasi elektron : [Kr] 4d^10 5s^2 5p^5
+				Titik lebur : 386,85 K
+				Titik didih : 457,4 K
+				
+				Nama : Astatin
+				Simbol : At
+				Nomor Atom : 85
+				Massa Atom : 210 u
+				konfigurasi elektron : [Xe] 4f^14 5d^10 6s^2 6p^5
+				Titik lebur : 575,15 K
+				Titik didih : 610,15 K
+				
+				
+				Gas Mulia
+				
+				
+				 
+				Nama : Helium
+				Simbol : He
+				Nomor Atom : 2
+				Massa Atom : 4,0026 u
+				konfigurasi elektron : 1s^2
+				Titik lebur : 0,95 K
+				Titik didih : 4,22 K
+				
+				Nama : Neon
+				Simbol : Ne
+				Nomor Atom : 10
+				Massa Atom : 10,180 u
+				konfigurasi elektron : 1s^2 2s^2 2p^6
+				Titik lebur : 24,56 K
+				Titik didih : 27,104 k
+				
+				Nama : Argon
+				Simbol : Ar
+				Nomor Atom : 18
+				Massa Atom : 39,95 u
+				konfigurasi elektron : [Ne] 3s^2 3p^6
+				Titik lebur : 83,81 K
+				Titik didih : 87,302 K
+				
+				Nama : Kripton
+				Simbol : Kr
+				Nomor Atom : 36
+				Massa Atom : 83,798 u
+				konfigurasi elektron : [Ar] 3d^10 4s^2 4^p6
+				Titik lebur : 115,78 K
+				Titik didih : 119,93 K
+				
+				Nama : Xenon
+				Simbol : Xe
+				Nomor Atom : 54
+				Massa Atom : 131,29
+				konfigurasi elektron : [Kr] 4d^10 5s^2 5p^6
+				Titik lebur : 161,40 K
+				Titik didih : 165,051 K
+				
+				Nama : Radon
+				Simbol : Rn
+				Nomor Atom : 86
+				Massa Atom : 222 u
+				konfigurasi elektron : [Xe] 4f^14 5d^10 6s^2 6p^6
+				Titik lebur : 202 K
+				Titik didih : 211,3 K
